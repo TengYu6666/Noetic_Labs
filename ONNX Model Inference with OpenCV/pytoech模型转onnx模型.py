d89a6c87d232aa4179aa0fe4828a2c612ce6d4b9
W <1571718920@qq.com>
2026-01-16 15:42:06 +0800

    Add files via upload

---
+++ ONNX Model Inference with OpenCV/pytoech模型转onnx模型.py
@@ -0,0 +1,18 @@
+import torch
+from torchvision import models
+
+# 有 GPU 就用 GPU，没有就用 CPU
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = torch.load('dataset101.pth')
+model = model.eval().to(device)
+x = torch.randn(1, 3, 256, 256).to(device)
+
+with torch.no_grad():
+    torch.onnx.export(
+        model,                   # 要转换的模型
+        x,                       # 模型的任意一组输入
+        'resnet18_dataset101.onnx', # 导出的 ONNX 文件名
+        opset_version=11,        # ONNX 算子集版本
+        input_names=['input'],   # 输入 Tensor 的名称（自己起名字）
+        output_names=['output']  # 输出 Tensor 的名称（自己起名字）
+    ) 
